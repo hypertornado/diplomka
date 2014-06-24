c06d6b7529f9f042ae98d7c53821b77475d20d1d
Ondrej Odchazel <hypertornado@gmail.com>
2014-06-24 09:31:43 +0200

    progress

--- public/js/js/diplomka/languages/trigrams.js
+++ public/js/js/diplomka/languages/trigrams.js
@@ -1,9 +1,31 @@
 goog.provide("oo.diplomka.Languages.Trigrams");
 
-goog.provide("oo.diplomka.Languages.TrigramsData");
+goog.require("oo.diplomka.Languages.TrigramsData");
 
 oo.diplomka.Languages.Trigrams = function () {
 
-  this.csData = oo.diplomka.Languages.TrigramsData.cs;
-  this.enData = oo.diplomka.Languages.TrigramsData.en;
+  this.lang_ = {};
+
+  this.lang_["cs"] = oo.diplomka.Languages.TrigramsData.cs;
+  this.lang_["en"] = oo.diplomka.Languages.TrigramsData.en;
 }
+
+oo.diplomka.Languages.Trigrams.prototype.detect = function(text) {
+  text = text.toLowerCase();
+  var trigrams = {};
+  for (var i = 0; i <= text.length - 3; i++) {
+    var trigram = text.substr(i, 3);
+    if (trigram in trigrams) {
+      trigrams[trigram] += 1;
+    } else {
+      trigrams[trigram] = 1;
+    }
+  }
+  var sorted = [];
+  for (var trigram in trigrams) sorted.push([trigram, trigrams[trigram]]);
+
+  sorted = sorted.sort(function(a, b) {return b[1] - a[1]});
+  
+  console.log(sorted);
+}
+
--- public/js/js/diplomka/languages/trigrams_test.js
+++ public/js/js/diplomka/languages/trigrams_test.js
@@ -2,7 +2,11 @@ goog.provide("oo.diplomka.Languages.TrigramsTest");
 
 goog.setTestOnly('oo.diplomka.Languages.TrigramsTest');
 goog.require('goog.testing.jsunit');
+goog.require("oo.diplomka.Languages.Trigrams");
 
 function testTrigrams() {
-  assertEquals(true, false);
+  assertEquals(true, true);
+  var trigrams = new oo.diplomka.Languages.Trigrams();
+  trigrams.detect("abababbab");
+
 }
